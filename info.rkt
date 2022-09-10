8941375b15bf915513afbb60f8ab0cc963488ee1
eutro <benedek.szilvasy@gmail.com>
2022-09-10 16:58:27 +0200

    Bump version

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/gdlisp.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.0")
+(define version "0.1")
 (define pkg-authors '(eutro))
 (define license '(Apache-2.0 OR MIT))
 (define raco-commands
