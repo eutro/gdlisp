67b81f9ab242fb5060f1b216aa2a0b9a28ee1375
eutro <benedek.szilvasy@gmail.com>
2022-11-03 22:41:39 +0000

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -3,8 +3,8 @@
 (define deps '("base" "syntax-classes-lib" "file-watchers" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/gdlisp.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.1")
+(define pkg-desc "A lisp dialect that compiles to GDScript.")
+(define version "0.2")
 (define pkg-authors '(eutro))
 (define license '(Apache-2.0 OR MIT))
 (define raco-commands
