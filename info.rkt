28b1f2ad53a45da7422d6de80189fef351220e09
eutro <benedek.szilvasy@gmail.com>
2022-11-04 07:59:51 +0000

    "Fix" failing tests (hopefully)

--- info.rkt
+++ info.rkt
@@ -4,9 +4,9 @@
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/gdlisp.scrbl" ())))
 (define pkg-desc "A lisp dialect that compiles to GDScript.")
-(define version "0.2")
+(define version "0.3")
 (define pkg-authors '(eutro))
 (define license '(Apache-2.0 OR MIT))
 (define raco-commands
-  '(("gdlisp" gdlisp/cli "Compile GDLisp files" #f)))
+  '(("gdlisp" (submod gdlisp/cli main) "Compile GDLisp files" #f)))
 
